c4bda1798d6e8bd6bc266caa9fca1ff6cb8ce005
samdirkx1 <271146@student.fontys.nl>
2017-08-29 14:18:12 +0200

    mainform opent nu

--- JCFLesson1/src/jcflesson1/JCFLesson1.java
+++ JCFLesson1/src/jcflesson1/JCFLesson1.java
@@ -5,11 +5,14 @@
  */
 package jcflesson1;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,25 +23,14 @@ import javafx.stage.Stage;
 public class JCFLesson1 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {
+       FXMLLoader myLoader = new FXMLLoader(getClass().getResource("MainForm.fxml"));
+
+       Pane myPane = (Pane)myLoader.load();
+
+       Scene myScene = new Scene(myPane);        
+       primaryStage.setScene(myScene);
+       primaryStage.show();
     }
 
     /**
