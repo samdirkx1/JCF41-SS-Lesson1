e1074b485a80b9d76e3b9363a0c1ba76f698d925
Berghs,Sander S.C.R.H. <s.berghs@student.fontys.nl>
2017-09-11 15:40:19 +0200

    WoordenAppTest

---
+++ JCFLesson1/test/TestPackage/WoordenAppTest.java
@@ -0,0 +1,26 @@
+package TestPackage;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sander
+ */
+public class WoordenAppTest {
+    
+    public WoordenAppTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+}
